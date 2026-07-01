9eb7c49e9c369dbc93af1312d84dc4a183c64463
l0dest <l0destpm@gmail.com>
2026-06-30 21:42:12 -0300

    puto el que lee

--- finoma/finoma.h
+++ finoma/finoma.h
@@ -9,6 +9,7 @@
 #define NUMERO_OPERACIONES 8
 // Para hacer DP Pura, necesitamos un universo finito. Limitamos el acumulador máximo.
 #define MAX_ESTADO 1000000 
+#define INF 999999
 #define MAX_DEPTH 15
 
 
